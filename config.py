8bec38e0634665ea9e55a81f95e86ecf6abd43ef
ropakolai <119304833+ropakolai@users.noreply.github.com>
2024-04-12 16:54:33 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,24 @@
+##
+
+paths = {
+    "main_path": ".",
+    "data_folder": "data",
+    "models_path": "models",
+    "csv_path":"csv",
+    "figures_path":"figures",    
+    "test_images_path":"test_images",
+    "external_images_path":"external_images",
+    "sm_subfolder":"classif_sm",
+    "cv_subfolder":"classif_cv",
+    "mc_subfolder":"classif_mc",
+    "3c_subfolder":"classif_3c"
+    }
+
+files = {
+    "covid_csv_name":"covid_info.csv"
+    }
+
+infolog = {
+    "logs_folder": "logs",
+    "logfile_name": "streamlit.log",
+}
